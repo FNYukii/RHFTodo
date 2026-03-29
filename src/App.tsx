1906fff8807f48e3eaa4188ee29dc39be66490d7
FNYukii <forestgreen14689@gmail.com>
2026-03-29 23:46:00 +0900

    feat: Footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,21 @@ import { CalendarSection } from './features/CalendarSection/CalendarSection'
 
 export const App = () => {
   return (
-    <div className="h-screen flex flex-col">
+    <div>
       <Header />
 
-      <div
-        className={clsx(
-          'mt-2 mb-8 container',
-          'grow',
-          'grid grid-cols-2 gap-6',
-        )}
-      >
-        <TodoSection className="h-full" />
+      <div className={clsx('mt-2 mb-8 container', 'grid grid-cols-2 gap-6')}>
+        <TodoSection />
 
         <div className="flex flex-col gap-6">
           <MemoSection />
           <CalendarSection className="aspect-square" />
         </div>
       </div>
+
+      <footer className="fixed bottom-0 left-0 right-0 pb-4 text-center text-disabled">
+        Copyright © 2026 FNYukii
+      </footer>
     </div>
   )
 }
